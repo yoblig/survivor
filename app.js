258122439c992c8b773faaa0d9744dc4088902f9
Kevin Gilboy <kgilboy@imgaineyourweb.com>
2017-09-14 16:26:32 -0500

    Change Port

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-  res.send('MONEY');
+  res.send('Survivor');
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log('Listening on 3000');
 })
