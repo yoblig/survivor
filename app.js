dd4db1f4d60c2099e06fd678556bb23fdc24bd37
Kevin Gilboy <kgilboy@imgaineyourweb.com>
2017-09-14 16:37:09 -0500

    Survivor Git Travis Heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-  res.send('Survivor Heroku');
+  res.send('Survivor -> Git -> Travis -> Heroku');
 });
 
 app.listen(process.env.PORT || 3000, function(){
